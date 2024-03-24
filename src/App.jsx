f68e603e28c6a79e9d439fb6253bbf9db0369237
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-03-24 18:02:05 +0200

    adding react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import BrowseJobs from "./components/BrowseJobs";
-import ViewAllJobs from "./components/ViewAllJobs";
-
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./Layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
 function App() {
-  return (
-    <>
-      <NavBar />
-      <Hero />
-      <HomeCards />
-      <BrowseJobs />
-      <ViewAllJobs />
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import BrowseJobs from "../components/BrowseJobs";
+import ViewAllJobs from "../components/ViewAllJobs";
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <HomeCards />
+      <BrowseJobs />
+      <ViewAllJobs />
+    </>
+  );
+}
+
+export default HomePage;
