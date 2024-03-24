9f1c5187a845d49cf4e510a105b9eb25ec7ba7fe
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-03-24 14:50:35 +0200

    NavBar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
 
 export default App;
